591858fd01339fb2ba510505781edc3e954750fc
karasyuk <karasyuk@promsoft.ru>
2022-06-02 11:11:40 +0700

    Classify MP3 results - fixed digits

--- get_pd_table_html.py
+++ get_pd_table_html.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 with open("result.md", 'w') as f:
-    pd.read_csv("classify_results/result_40000.csv", sep=';').describe().to_html(f)
+    pd.read_csv("classify_results/result_5000.csv", sep=';', header=None).describe().round(2).to_html(f)
